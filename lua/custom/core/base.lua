695b9868e843bb1fc98f9c024a941a0e6e2ddb45
Cyan <77025112+shadycyan@users.noreply.github.com>
2024-09-28 22:06:01 +0330

    feat: add basic vim configurations

---
+++ lua/custom/core/base.lua
@@ -0,0 +1,9 @@
+-- Tabs & Indentation
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+-- Use relative line numbers
+vim.opt.relativenumber = true
