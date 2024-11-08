3814f69d23244b57547028167b4e501727e18448
Cyan <77025112+shadycyan@users.noreply.github.com>
2024-11-09 01:31:09 +0330

    chore: update base opts

--- lua/custom/core/base.lua
+++ lua/custom/core/base.lua
@@ -1,9 +1,14 @@
--- Tabs & Indentation
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
 vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
-vim.opt.autoindent = true
+
 vim.opt.smartindent = true
 
--- Use relative line numbers
-vim.opt.relativenumber = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.termguicolors = true
