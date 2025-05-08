9d7045469b505b8b805686be29d474d2f383edc9
Cyan <77025112+shadycyan@users.noreply.github.com>
2025-05-08 15:50:13 +0330

    chore: change colorscheme

--- lua/custom/plugins/colors.lua
+++ lua/custom/plugins/colors.lua
@@ -54,7 +54,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      -- ColorMyPencils()
+      ColorMyPencils 'catppuccin'
     end,
     opts = {
       flavour = 'mocha',
@@ -102,7 +102,7 @@ return {
         },
       }
 
-      -- ColorMyPencils()
+      -- ColorMyPencils 'dracula'
     end,
   },
 
@@ -111,21 +111,54 @@ return {
     priority = 1000,
     config = function()
       require('tokyonight').setup {
-        style = 'storm',
-        -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-        transparent = true,
-        terminal_colors = true,
+        style = 'moon', -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+        light_style = 'day', -- The theme is used when the background is set to light
+        transparent = true, -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
         styles = {
-          comments = { italic = false },
-          keywords = { italic = false },
-          sidebars = 'dark',
-          floats = 'dark',
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = true },
+          keywords = { italic = true },
+          functions = {},
+          variables = {},
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = 'dark', -- style for sidebars, see below
+          floats = 'dark', -- style for floating windows
         },
-        on_colors = function(_) end,
-        on_highlights = function(_, _) end,
+        day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+        dim_inactive = false, -- dims inactive windows
+        lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+
+        --- You can override specific color groups to use other groups or a hex color
+        --- function will be called with a ColorScheme table
+        ---@param colors ColorScheme
+        on_colors = function(colors) end,
+
+        --- You can override specific highlights to use other groups or a hex color
+        --- function will be called with a Highlights and ColorScheme table
+        ---@param highlights tokyonight.Highlights
+        ---@param colors ColorScheme
+        on_highlights = function(highlights, colors) end,
+
+        cache = true, -- When set to true, the theme will be cached for better performance
+
+        ---@type table<string, boolean|{enabled:boolean}>
+        plugins = {
+          -- enable all plugins when not using lazy.nvim
+          -- set to false to manually enable/disable plugins
+          all = package.loaded.lazy == nil,
+          -- uses your plugin manager to automatically enable needed plugins
+          -- currently only lazy.nvim is supported
+          auto = true,
+          -- add any plugins here that you want to enable
+          -- for all possible plugins, see:
+          --   * https://github.com/folke/tokyonight.nvim/tree/main/lua/tokyonight/groups
+          -- telescope = true,
+        },
       }
 
-      -- ColorMyPencils()
+      -- ColorMyPencils 'tokyonight-moon'
     end,
   },
 
@@ -147,7 +180,7 @@ return {
     'RRethy/base16-nvim',
     priority = 1000,
     config = function()
-      ColorMyPencils 'base16-black-metal'
+      -- ColorMyPencils 'base16-black-metal'
     end,
   },
 }
